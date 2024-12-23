85aa6f0a53517b73c3240674b3ca21ff4ef566ad
Eflowz <ezekielkachy@gmail.com>
2024-12-23 08:29:48 -0800

    fallback

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import React, { useEffect, Suspense } from 'react';
 import First from "../Components/First";
 import Sech from "../Components/Sech";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import ServicesIntro from "../Components/ServicesIntro";
 import Blum from '../Components/Blum';
 import Footer from "../Components/Footer";
 import Foot from "../Components/Foot";
-
 const Frisbee = React.lazy(() => import('../Components/Frisbee'));
 
 const Home = () => {
-  useEffect(() => {
-    AOS.init({ duration: 1000 });
-  }, []);
-
+  useEffect(()=> {
+    AOS.init({duration:1000});
+  }, [])
   return (
     <div>
       <Suspense fallback={<div>Loading video...</div>}>
